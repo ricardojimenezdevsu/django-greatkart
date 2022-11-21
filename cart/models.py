559dd4ca56b12bf54c1c0e5e1224c6089b21e105
Ricardo Jimenez E <ricardo.jimenez@devsu.com>
2022-11-21 12:12:58 -0700

    Now can update the items in cart

--- cart/models.py
+++ cart/models.py
@@ -17,4 +17,7 @@ class CartItem(models.Model):
     is_active = models.BooleanField(default=True)
 
     def __str__(self):
-        return self.product
+        return self.product.product_name
+
+    def subtotal(self):
+        return self.quantity * self.product.price
