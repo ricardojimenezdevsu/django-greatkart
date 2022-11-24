825cf1280b00ea96fdd67dcc46ef61cf3d8b549d
Ricardo Jimenez E <ricardo.jimenez@devsu.com>
2022-11-24 13:41:39 -0700

    Config s3

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -29,8 +29,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = [
-    'greatkart-course-env.eba-yepszwge.us-west-2.elasticbeanstalk.com']
+ALLOWED_HOSTS = ['greatkart-course-env.eba-yepszwge.us-west-2.elasticbeanstalk.com','*']
 
 
 # Application definition
@@ -48,7 +47,8 @@ INSTALLED_APPS = [
     'store',
     'cart',
     'order',
-    'admin_honeypot'
+    'admin_honeypot',
+    'storages'
 ]
 
 MIDDLEWARE = [
@@ -151,12 +151,29 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+# STATIC_URL = 'static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, '/')
+# STATIC_ROOT = os.path.join(BASE_DIR, '/')
 
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'greatkart/static'), )
+# STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), )
+# AWS S3 Static Files Configuration
+AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400',
+}
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = 'public-read'
+AWS_LOCATION = 'static'
 
+STATICFILES_DIRS = [
+    'greatkart/static',
+]
+STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+DEFAULT_FILE_STORAGE = 'greatkart.media_storages.MediaStorage'
 # media files configuration
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
